89bb80eb1dbf7c55ffa0064bdcf1ab36135b93f5
Shivani Nagda <142904884+ShivaniNagda@users.noreply.github.com>
2025-12-28 13:20:49 +0530

    Update LogoSection styling and image height

--- src/sections/LogoSection.jsx
+++ src/sections/LogoSection.jsx
@@ -5,8 +5,8 @@ const LogoSection = () => {
 
     const LogoICon = ({icon} ) =>{
         return(
-            <div className='flex-none flex-center marquee-item  '>
-                <img src={icon.imgPath} alt={icon.name} className='w-38 h-20 object-contains ' />
+            <div className='flex-none flex-center marquee-item  border-card bg-white/20 rounded-lg p-2 md:w-48 w-32 h-28 md:h-48 '>
+                <img src={icon.imgPath} alt={icon.name} className='w-38 h-26 object-contains  ' />
             </div>
         )
     }
